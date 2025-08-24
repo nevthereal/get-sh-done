422a4c40eb2293bd81af768a353feddb315a93b9
Neville Brem <contact@nevillebrem.com>
2025-08-24 15:03:47 +0200

    added shad stuff, init of main page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+	import { ModeWatcher } from 'mode-watcher';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<ModeWatcher defaultMode="dark" />
+
+<nav class="flex gap-2 p-4">
+	<h1 class="text-4xl font-bold">get (sh)it done</h1>
+</nav>
+<div class="mx-auto max-w-3xl">
+	{@render children?.()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { CornerRightUp } from '@lucide/svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import Input from '$lib/components/ui/input/input.svelte';
+</script>
+
+<div class="flex gap-2">
+	<Input class="w-full pr-12" />
+	<Button size="icon"><CornerRightUp /></Button>
+</div>
