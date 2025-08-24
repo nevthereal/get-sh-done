d1ee5572e7449ac0713baf6e156d3adb9701f505
Neville Brem <contact@nevillebrem.com>
2025-08-24 19:27:14 +0200

    add goal breakdown

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,56 @@
 <script>
-	import { CornerRightUp } from '@lucide/svelte';
-	import { smoothStream } from 'ai';
+	import * as HoverCard from '$lib/components/ui/hover-card/index.js';
+	import * as Card from '$lib/components/ui/card';
+	import { CornerRightUp, Info } from '@lucide/svelte';
 	import Button from '$lib/components/ui/button/button.svelte';
 	import Input from '$lib/components/ui/input/input.svelte';
-	import { Chat } from '@ai-sdk/svelte';
-	import { callAi } from './shit.remote';
+	import { generateSteps } from './shit.remote';
 
-	// const chat = new Chat({});
+	const generatedRes = $derived(generateSteps.result);
 </script>
 
-<form {...callAi} class="flex gap-2">
-	<Input name="prompt" class="w-full pr-12" />
-	<Button size="icon"><CornerRightUp /></Button>
+<h1 class="my-16 text-center text-4xl font-black">What big task or goal lies ahead of you?</h1>
+<form {...generateSteps} class="flex gap-2">
+	<Input name="prompt" placeholder="Just state it. We will break it down..." class="w-full pr-12" />
+	<Button type="submit" size="icon"><CornerRightUp /></Button>
 </form>
+
+<div class="my-8 grid grid-cols-2 gap-4">
+	{#if generatedRes}
+		{#each generatedRes as res, idx (idx)}
+			<Card.Root class="flex flex-col justify-between">
+				<div class="flex flex-col gap-2">
+					<Card.Header>
+						<Card.Title>Small Step N° {idx}</Card.Title>
+					</Card.Header>
+					<Card.Content class="flex items-center justify-between gap-2">
+						<span>
+							{res.title}
+							{#if res.isHabit}
+								<span class="text-primary"> (habit) </span>
+							{/if}
+						</span>
+						<HoverCard.Root>
+							<HoverCard.Trigger>
+								<Info size={20} />
+							</HoverCard.Trigger>
+							<HoverCard.Content
+								><p>
+									{res.description}
+								</p>
+								{#if res.isHabit && res.howOften}
+									<p class="text-muted-foreground">
+										Should be repeated {res.howOften}
+									</p>
+								{/if}
+							</HoverCard.Content>
+						</HoverCard.Root>
+					</Card.Content>
+				</div>
+				<Card.Footer>
+					<Button class="w-full" variant="destructive">Reject</Button>
+				</Card.Footer>
+			</Card.Root>
+		{/each}
+	{/if}
+</div>
--- src/routes/shit.remote.ts
+++ src/routes/shit.remote.ts
@@ -1,6 +1,6 @@
 import { form } from '$app/server';
-import { createOpenAI } from '@ai-sdk/openai';
-import { generateText, streamText } from 'ai';
+import { z } from 'zod';
+import { generateObject } from 'ai';
 import { error } from '@sveltejs/kit';
 import { OPENAI_KEY } from '$env/static/private';
 import { createGateway } from '@ai-sdk/gateway';
@@ -9,17 +9,28 @@ const gateway = createGateway({
 	apiKey: OPENAI_KEY
 });
 
-export const callAi = form(async (formData) => {
+export const generateSteps = form(async (formData) => {
 	const prompt = formData.get('prompt');
 
 	if (!prompt) return error(400, 'No prompt');
 
-	const res = streamText({
-		model: gateway('openai/gpt-5'),
-		prompt: prompt.toString()
+	const res = await generateObject({
+		model: gateway('openai/gpt-5-mini'),
+		prompt: prompt.toString(),
+		schema: z.object({
+			title: z.string().describe('Title of the steps necessary to take'),
+			isHabit: z.boolean().describe('If the step should be repeated or not'),
+			description: z.string('More precise description of the task'),
+			howOften: z
+				.enum(['daily', 'weekly', 'hourly', 'monthly'])
+				.nullable()
+				.describe('If a habit, how often should the step be repeated')
+		}),
+		output: 'array',
+		maxRetries: 5,
+		system:
+			'You are an assistant who processes the prompt, which optimally should be a goal or big task and should turn it into smaller, swallowable doable steps.'
 	});
 
-	for await (const textPart of res.textStream) {
-		console.log(textPart);
-	}
+	return res.object;
 });
